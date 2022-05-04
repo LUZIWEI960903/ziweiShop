780694f0db3c64ec76b94309dd0cc7921d74629c
ziwei <626398871@qq.com>
2022-05-04 18:49:34 +0800

    feat: add func redis Set & Get

---
+++ dao/redis/redisCache.go
@@ -0,0 +1,31 @@
+package redis
+
+import (
+	"encoding/json"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+var CacheDB = &cacheDB{}
+
+type cacheDB struct {
+}
+
+func (c cacheDB) Set(key string, value interface{}, expiration int) {
+	v, err := json.Marshal(value)
+	if err != nil {
+		zap.L().Error("[pkg: redis] [func: (c CacheDB) Set(key string, value interface{}, expiration int)] [json.Marshal(value)] failed, ", zap.Error(err))
+	} else {
+		rdb.Set(ctx, key, string(v), time.Second*time.Duration(expiration))
+	}
+}
+
+func (c cacheDB) Get(key string, obj interface{}) bool {
+	v, err := rdb.Get(ctx, key).Result()
+	if err == nil && v != "" {
+		err1 := json.Unmarshal([]byte(v), obj)
+		return err1 == nil
+	}
+	return false
+}
